ae5c33fc9c420a9e938eb571d0e61f5168559952
Stephen Chai <stephenxp04@gmail.com>
2024-07-22 10:47:51 +0800

    add cloudflare ip config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :log_request_info
+
+  private
+
+  def log_request_info
+    Rails.logger.info "Remote IP: #{request.remote_ip}"
+    Rails.logger.info "X-Forwarded-For: #{request.headers['X-Forwarded-For']}"
+    Rails.logger.info "Original Fullpath: #{request.original_fullpath}"
+  end
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,10 @@ Rails.application.configure do
   # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
+
+  # Trust the X-Forwarded-Proto header from Cloudflare
+  config.ssl_options = { redirect: { exclude: -> request { request.headers['X-Forwarded-Proto'] == 'https' } } }
 
   # Info include generic and useful information about system operation, but avoids logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII). If you
@@ -86,7 +89,23 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.middleware.insert_before(0, ActionDispatch::RemoteIp, 
-  trusted_proxies: ['127.0.0.1', '::1', '13.213.42.233'])
-  config.action_dispatch.trusted_proxies = ['127.0.0.1', '::1', '13.213.42.233']
+  cloudflare_ips = %w(
+    173.245.48.0/20
+    103.21.244.0/22
+    103.22.200.0/22
+    103.31.4.0/22
+    141.101.64.0/18
+    108.162.192.0/18
+    190.93.240.0/20
+    188.114.96.0/20
+    197.234.240.0/22
+    198.41.128.0/17
+    162.158.0.0/15
+    104.16.0.0/13
+    104.24.0.0/14
+    172.64.0.0/13
+    131.0.72.0/22
+  )
+  config.middleware.insert_before(0, ActionDispatch::RemoteIp, trusted_proxies: ['127.0.0.1', '::1', '13.213.42.233'] + cloudflare_ips)
+  config.action_dispatch.trusted_proxies = ['127.0.0.1', '::1', '13.213.42.233'] + cloudflare_ips
 end
